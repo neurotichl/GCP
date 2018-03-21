28cd31cb7fee0e983ab07c1c18587be8015291f9
Neurotic <chiauhung.lee@gmail.com>
2018-03-21 18:18:52 +0800

    Add memcache

--- AppEngine/standard_local/main.py
+++ AppEngine/standard_local/main.py
@@ -1,5 +1,5 @@
-from flask import Flask, redirect, url_for
-from google.appengine.api import taskqueue
+from flask import Flask, redirect, url_for, request, render_template
+from google.appengine.api import taskqueue, memcache
 from google.appengine.ext import ndb
 from datetime import datetime 
 import numpy as np
@@ -8,6 +8,12 @@ import time
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+	return "Hello World!"
+
+################################## NDB DATASTORE ##########################################
+
 class HelloWorld(ndb.Model):
 	# https://cloud.google.com/appengine/docs/standard/python/ndb/
 	Date = ndb.DateTimeProperty(indexed=True)
@@ -15,20 +21,18 @@ class HelloWorld(ndb.Model):
 	Integer = ndb.IntegerProperty(indexed=True)
 	Float = ndb.FloatProperty(indexed=True)
 
-@app.route('/')
-def hello_world():
-	return "Hello World!"
-
 @app.route('/insert')
 def insert_datastore():
 
 	now = datetime.now()
 	hello_data = HelloWorld(Date = now,
-							Text='hello world!',
-							Integer= now.hour,
-							Float=np.random.rand(1)[0])
+							Text = 'hello world!',
+							Integer = now.hour,
+							Float = np.random.rand(1)[0])
 	hello_data.put()
 	return 'Data inserted to datastore at {}'.format(now), 200
+
+################################## TASK QUEUE #############################################
 
 @app.route('/queue')
 def queue_task():
@@ -41,7 +45,7 @@ def queue_task():
 	rpc = queue.add_async(task)
 	task = rpc.get_result()
 	logging.info(task)
-	return "queue"
+	return "queue {}".format(task.name)
 
 @app.route('/delay_insert')
 def delay():
@@ -49,11 +53,55 @@ def delay():
 	time.sleep(5)
 	logging.info('task queue at time {}'.format(now.isoformat()))
 	hello_data = HelloWorld(Date = now,
-							Text='queued hello world!',
-							Integer= now.hour,
-							Float=np.random.rand(1)[0])
+							Text = 'queued hello world!',
+							Integer = now.hour,
+							Float = np.random.rand(1)[0])
 	hello_data.put()
 	return 'Data inserted from queue to datastore at {}'.format(now), 200
+
+
+################################## MEMCACHE ###############################################
+m = memcache.Client()
+
+@app.route('/cache', methods=['GET','POST'])
+def name():
+	print 'Call name'
+	if request.method == 'POST':
+		name = request.form['Name']
+
+		value = m.get(name)
+		if not value:
+			return render_template('whatmood.html', name=name)
+		elif value:
+			return render_template('updatemood.html', name=name, mood=value)
+
+	return render_template('mood.html')
+
+@app.route('/mood', methods=['POST'])
+def current_mood():
+
+	name = request.form['Name']
+	mood = request.form['mood']
+	
+	if request.form['submit_mood'] == 'Update':
+		m.get(name, for_cas=True)
+		m.cas(name,mood) # Update existing memcache value
+	elif request.form['submit_mood'] == 'Forget about me':
+		m.delete(name)
+		return "<html><p>Goodbye... Unknown guest</p><a href=/cache>Go back</a></html>"
+	else:
+		m.add(name, mood, 60) # key, value, timeout
+
+	return '''
+	<html>
+		<body>
+			<p>Hi {}, You are feeling {} now...</p>
+			<a href=/cache>Go back</a>
+		</body>
+	</html>
+	'''.format(name, mood)
+
+
 
 if __name__=='__main__':
 	app.run()
